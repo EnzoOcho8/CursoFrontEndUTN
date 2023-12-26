994978a59ba22bd81aa309b07d4d1048668b12d3
EnzoOcho8 <123573929+EnzoOcho8@users.noreply.github.com>
2023-12-26 09:02:03 -0300

    Add files via upload

---
+++ M4-U4/routes/admin/login.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require("./../../models/usuariosModel");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login',{
+    layout:"admin/layout"
+  });
+});
+
+//cerrar sesion
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+      layout: 'admin/layout'
+  })
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+
+        
+        console.log(req.body);
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        console.log(req.body);
+
+        var data = await usuariosModel.getUserAndPassword (usuario, password);
+
+        if (data != undefined) {
+            req.session.id_usuario = data.id;
+            req.session.nombre = data.usuario;
+
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            })
+        } //cierre else
+    } catch (error) {
+        console.log(error)
+    }
+  })
+
+module.exports = router;
